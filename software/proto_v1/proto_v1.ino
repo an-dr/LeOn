a632e5d96b6eca99e9ad6c9f8e415e7cf65f202f
Andrei Gramakov <mail@agramakov.me>
2023-11-13 21:57:35 +0100

    Update Proto 1

--- software/proto_v1/proto_v1.ino
+++ software/proto_v1/proto_v1.ino
@@ -1,53 +1,74 @@
-/* Example sketch to control a 28BYJ-48 stepper motor with ULN2003 driver board and Arduino UNO. More info: https://www.makerguides.com */
-
-// Include the Arduino Stepper.h library:
+//Includes the Arduino Stepper Library
 #include <Stepper.h>
+#include <CheapStepper.h>
 
-// Define number of steps per rotation:
-const int stepsPerRevolution = 2048;
+// Defines the number of steps per rotation
+const int stepsPerRevolution = 2038;
 
-// Wiring:
-// Pin 8 to IN1 on the ULN2003 driver
-// Pin 9 to IN2 on the ULN2003 driver
-// Pin 10 to IN3 on the ULN2003 driver
-// Pin 11 to IN4 on the ULN2003 driver
+Stepper StepperR = Stepper(stepsPerRevolution, 8, 10, 9, 11);
+Stepper StepperL = Stepper(stepsPerRevolution, 4, 6, 5, 7);
 
-// Create stepper object called 'myStepper', note the pin order:
-Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
-Stepper myStepper2 = Stepper(stepsPerRevolution, 4, 5, 6, 7);
+class DcMotor {
+  int m_pin_a;
+  int m_pin_b;
+
+  DcMotor() = default;
+
+public:
+  DcMotor(int pin_a, int pin_b)
+    : m_pin_a(pin_a), m_pin_b(pin_b) {
+    pinMode(m_pin_a, OUTPUT);
+    pinMode(m_pin_b, OUTPUT);
+  };
+
+  void direction(bool clock, uint32_t speed) {
+    if (clock) {
+      analogWrite(m_pin_a, speed);
+      analogWrite(m_pin_b, 0);
+      // digitalWrite(m_pin_a, 1);
+      // digitalWrite(m_pin_b, 0);
+
+    } else {
+      analogWrite(m_pin_a, 0);
+      analogWrite(m_pin_b, speed);
+      // digitalWrite(m_pin_a, 0);
+      // digitalWrite(m_pin_b, 1);
+    }
+  }
+
+  void stop() {
+    analogWrite(m_pin_a, 0);
+    analogWrite(m_pin_b, 0);
+  }
+};
+
+DcMotor dc = DcMotor(2, 3);
+uint32_t time_store = 0;
 
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  // Nothing to do (Stepper Library sets pins as outputs)
 
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  // Set the speed to 5 rpm:
-  myStepper.setSpeed(1);
-  myStepper2.setSpeed(1);
-
-  
-  // Begin Serial communication at a baud rate of 9600:
-  Serial.begin(9600);
+  StepperL.setSpeed(10);
+  StepperR.setSpeed(10);
+  time_store = millis();
 }
 
 void loop() {
-  // Step one revolution in one direction:
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
-  myStepper2.step(stepsPerRevolution);
 
-  delay(500);
-  
-  // Step one revolution in the other direction:
-  Serial.println("counterclockwise");
-  myStepper.step(-stepsPerRevolution);
-  delay(500);
+  // Rotate CCW quickly at 10 RPM
+  StepperR.step(1);
+  StepperL.step(1);
+
+  int now = millis();
+  if ((now - time_store > 3000) && (now - time_store <= 3400)) {
+    dc.direction(1, 180);
+  }
+
+  if (now - time_store > 3400) {
+    dc.stop();
+    time_store = millis();
+  }
+
+
+  // delay(1000);
 }
