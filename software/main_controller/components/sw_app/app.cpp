5febef97e2d3149f30d24b0b15add5d4a56e6014
Andrei Gramakov <mail@agramakov.me>
2023-11-27 00:08:51 +0100

    Refactor app

--- software/main_controller/components/sw_app/app.cpp
+++ software/main_controller/components/sw_app/app.cpp
@@ -8,28 +8,38 @@
 
 using namespace hal;
 
+static uint32_t last_time = GetUptime_ms();
+
+static void taskOnceToy(IToy &toy)
+{
+    uint32_t now = GetUptime_ms();
+
+    if ((now - last_time > 3000) &&
+        (now - last_time <= 3400))
+    {
+        toy.MoveRight(180);
+    }
+
+    if (now - last_time > 3400)
+    {
+        toy.Stop();
+        last_time = GetUptime_ms();
+    }
+}
+
+static void taskOncePlatform(IMovingPlatform &platform)
+{
+    platform.taskOnce();
+}
+
 int app(IToy &toy, IMovingPlatform &platform, uint32_t period_ms)
 {
     platform.SetSpeed(0, 0, 0, 0, 0, 10);
 
-    uint32_t last_time = GetUptime_ms();
     while (1)
     {
-        platform.taskOnce();
-
-        uint32_t now = GetUptime_ms();
-
-        if ((now - last_time > 3000) &&
-            (now - last_time <= 3400))
-        {
-            toy.MoveRight(180);
-        }
-
-        if (now - last_time > 3400)
-        {
-            toy.Stop();
-            last_time = GetUptime_ms();
-        }
+        taskOncePlatform(platform);
+        taskOnceToy(toy);
         Delay(period_ms);
     }
 }
